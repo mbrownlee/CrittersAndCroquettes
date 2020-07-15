2d52ad5485c7aac47d7b2541eb4159060c8ee316
mbrownlee <michelle@orbwave.com>
2020-07-15 16:43:03 -0500

    chapter 11

--- animals.py
+++ animals.py
@@ -2,69 +2,72 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
 
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
 
-miss_fuzz = Llama("Miss Fuzz", "domestic llama")
-for prop, value in miss_fuzz.__dict__.items():
-    print(f'{prop}:\n{value}\n')
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "midday")
+print(f'{miss_fuzz.name}, the {miss_fuzz.species}, is available to pet during the {miss_fuzz.shift} shift.')
 
 
 class Goat:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
 
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-fuzz_butt = Goat("Fuzz Butt", "cutest baby goat ever")
-print(fuzz_butt.name)
-
+fuzz_butt = Goat("Fuzz Butt", "cutest baby goat ever", "morning")
+print(f'{fuzz_butt.name}, the {fuzz_butt.species}, is available to pet during the {fuzz_butt.shift} shift.')
 
 class Bunny:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
 
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-bugs = Bunny("Bugs", "wraskly rabbit")
-print(bugs.name)
+bugs = Bunny("Bugs", "wraskly rabbit", "afternoon")
+print(f'{bugs.name}, the {bugs.species}, is available to pet during the {bugs.shift} shift.')
 
 
 class Bobcat:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
 
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-rufus = Bobcat("Rufus", "best party cat")
-print(rufus.name)
+rufus = Bobcat("Rufus", "best party cat", "morning")
+print(f'{rufus.name}, the {rufus.species}, is available to pet during the {rufus.shift} shift.')
 
 
 class Ostrich:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
 
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
 
-jerkface = Ostrich("Jerk Face", "biggest jerkiest bird ever")
-print(jerkface.name)
+jerkface = Ostrich("Jerk Face", "biggest jerkiest bird ever", "afternoon")
+print(f'{jerkface.name}, the {jerkface.species}, is available to pet during the {jerkface.shift} shift.')
 
 
 class Python:
